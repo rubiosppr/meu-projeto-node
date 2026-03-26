9b595556abb3faf147ab2b4923880aee9654f3b0
Mateus <mateusd@alunos.utfpr.edu.br>
2026-03-26 17:23:12 -0300

    var

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const { addition, subtraction, multiplication, division } = require('./operacoes');
-const lodash = require('lodash');
+const { adicao, subtracao, multiplicacao, divisao } = require("./operacoes");
+const lodash = require("lodash");
 
-console.log('8 + 4 =', addition(8, 4));
-console.log('15 - 7 =', subtraction(15, 7));
-console.log('6 * 3 =', multiplication(6, 3));
-console.log('20 / 5 =', division(20, 5));
-console.log('10 / 0 =', division(10, 0));
-console.log('Número aleatório entre 1 e 30:', lodash.random(1, 30));
+console.log("8 + 4 =", adicao(8, 4));
+console.log("15 - 7 =", subtracao(15, 7));
+console.log("6 * 3 =", multiplicacao(6, 3));
+console.log("20 / 5 =", divisao(20, 5));
+console.log("10 / 0 =", divisao(10, 0));
+console.log("Número aleatório entre 1 e 30:", lodash.random(1, 30));
--- operacoes.js
+++ operacoes.js
@@ -1,25 +1,25 @@
-function addition(a, b) {
-    return a + b;
+function adicao(a, b) {
+  return a + b;
 }
 
-function subtraction(a, b) {
-    return a - b;
+function subtracao(a, b) {
+  return a - b;
 }
 
-function multiplication(a, b) {
-    return a * b;
+function multiplicacao(a, b) {
+  return a * b;
 }
 
-function division(a, b) {
-    if (b === 0) {
-        return "Error: divisão por zero!";
-    }
-    return a / b;
+function divisao(a, b) {
+  if (b === 0) {
+    return "Erro: divisão por zero!";
+  }
+  return a / b;
 }
 
 module.exports = {
-    addition,
-    subtraction,
-    multiplication,
-    division
+  adicao,
+  subtracao,
+  multiplicacao,
+  divisao,
 };
